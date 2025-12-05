457f01efdad6bbd1ac8b7fd92e0655e316177783
NyverZ <nyverzthurfah@example.com>
2025-12-06 07:45:20 +0800

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
+import { PrismaModule } from './indra/database/prisma.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+    envFilePath: '.env',
+  }), 
+  ProductModule,
+  PrismaModule,
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
